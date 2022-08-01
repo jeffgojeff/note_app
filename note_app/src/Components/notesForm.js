1dc72ae1931a87c58bbc06ac0be9f5ac3a047060
jeffgojeff <jeffgojeff@gmail.com>
2022-07-31 22:23:27 -0500

    cleaned up redundant functions
    passed setModal as a prop and removed handleOk && handlCancle functions

--- note_app/src/Components/notesForm.js
+++ note_app/src/Components/notesForm.js
@@ -3,8 +3,7 @@ import {  Form, Input, Button } from 'antd';
 const { TextArea } = Input
 
 
-function NotesForm(props) {    
-
+function NotesForm(props) {
     return (
     <Form onFinish={props.onFinish} labelCol={{span: 4}} wrapperCol={{span: 16}} form={props.form}>
         <Form.Item 
@@ -20,8 +19,8 @@ function NotesForm(props) {
             <TextArea rows={4}/>
         </Form.Item>
         <Form.Item wrapperCol={{offset: 8,  span: 16 }}>
-            <Button type="primary" htmlType="submit" onClick={props.handleOk} style={{marginRight: 10}}> Save </Button>
-            <Button key="back" onClick={props.handleCancel}>Cancel</Button>
+            <Button type="primary" htmlType="submit" onClick={() => props.set(false)} style={{marginRight: 10}}> Save </Button>
+            <Button key="back" onClick={() => props.set(false)}>Cancel</Button>
         </Form.Item>
     </Form>
     )
